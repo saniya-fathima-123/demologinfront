062e8d68cc456a8940b87d3fe5aa70140d828358
saniya fathima <saniya.fathima@cabto.in>
2024-06-07 15:53:31 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+interface User {
+  categoryName: string;
+}
+const App: React.FC = () => {
+  const [regUsername, setRegUsername] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
 
-function App() {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/v1/category/read"
+      );
+      setUsers(response.data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+  console.log(users);
+  const handleRegister = async () => {
+    try {
+      console.log("hasan");
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/category/create",
+        {
+          categoryName: regUsername,
+        }
+      );
+      setRegUsername("");
+      fetchUsers();
+      const data = response.data;
+      console.log(data);
+    } catch (error: any) {
+      alert(`Error: ${error.message}`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Category Page</h1>
+      <input
+        type="text"
+        placeholder="Category Name"
+        value={regUsername}
+        onChange={(e) => setRegUsername(e.target.value)}
+      />
+
+      <br />
+      <button onClick={handleRegister}>Create Category</button>
+
+      <table style={{ margin: "0 auto", marginTop: "50px" }}>
+        <thead>
+          <tr>
+            <th>Category Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.categoryName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
